88952150c420db5f16a682b1eab4a55cb93936d7
yaseensiddiqui36 <yaseensiddiqui36@gmail.com>
2025-01-25 12:30:34 -0500

    prediction pipeline library updated

---
+++ src/pipelines/prediction_pipeline.py
@@ -0,0 +1,9 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
